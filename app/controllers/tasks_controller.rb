9a99be57d02371d18caf55629cb88efbb08c4a40
Akash Manohar J <akash@akash.im>
2011-04-09 07:22:07 +0530

    prepared Tasks controller to handle xhr requests only
    Signed-off-by: Akash Manohar J <akash@akash.im>

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,8 +1,30 @@
 class TasksController < ApplicationController
+  before_filter :allow_only_xhr
+  
+  respond_to :json
+  
   def create
+    @task = Task.new params[:task]
+    @task.save
+    respond_with @task
   end
 
   def update
+    @task = Task.find params[:id]
+    @task.update_attributes(params[:task])
+    respond_with @task
   end
 
+  def destroy
+    @task = Task.find params[:id]
+    @task.destroy
+  end
+  
+  private
+  def allow_only_xhr
+    unless request.xhr?
+      render :status => 404
+    end
+  end
+  
 end
