0b5e9a06a533852d3c681ea4e8e6a0fff1584d83
Akash Manohar J <akash@akash.im>
2011-04-09 06:50:39 +0530

    added after_create callback to Todo model
    Signed-off-by: Akash Manohar J <akash@akash.im>

--- Gemfile
+++ Gemfile
@@ -10,7 +10,7 @@ gem 'jquery-rails'
 gem 'haml'
 gem 'compass'
 gem 'fancy-buttons'
-
+gem 'base32'
 
 # Use unicorn as the web server
 # gem 'unicorn'
--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,3 +1,20 @@
 class Todo < ActiveRecord::Base
   has_many :tasks, :dependent=>:destroy
+
+  accepts_nested_attributes_for :tasks, :reject_if => lambda { |a| a[:name].blank? }, :allow_destroy => true
+
+  after_create :create_url_hash
+
+  def create_url_hash
+    if self.id.to_s.length == 1
+      taskID = "0" << self.id.to_s
+    else
+      taskID = self.id.to_s
+    end
+    
+    url_hash = Base32.encode(taskID).gsub("=", "")
+    self.update_attribute(:url_hash, url_hash)
+  end
+  
+  
 end
