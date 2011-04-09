c57560ec3d6061b5b91672543741dc5188011ea3
Akash Manohar J <akash@akash.im>
2011-04-09 07:11:51 +0530

    some more cleanup
    Signed-off-by: Akash Manohar J <akash@akash.im>

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -2,20 +2,10 @@ class TodosController < ApplicationController
 
   def show
     @todo = Todo.find_by_url_hash(params[:url_hash])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.xml  { render :xml => @todo }
-    end
   end
 
   def new
     @todo = Todo.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.xml  { render :xml => @todo }
-    end
   end
 
 
@@ -32,14 +22,10 @@ class TodosController < ApplicationController
   def update
     @todo = Todo.find(params[:id])
 
-    respond_to do |format|
-      if @todo.update_attributes(params[:todo])
-        format.html { redirect_to(@todo, :notice => 'Todo was successfully updated.') }
-        format.xml  { head :ok }
-      else
-        format.html { render :action => "edit" }
-        format.xml  { render :xml => @todo.errors, :status => :unprocessable_entity }
-      end
+    if @todo.update_attributes(params[:todo])
+      redirect_to(@todo, :notice => 'Todo was successfully updated.')
+    else
+      render :action => "edit"
     end
   end
 
